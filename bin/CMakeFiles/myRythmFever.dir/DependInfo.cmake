85de27e23dc8a2e1e1a644e01147df273b141fd3
Maximilian Siess <hunterwolf0815@gmail.com>
2016-01-07 12:50:00 +0100

    Trying to change to cmake. Not successful yet

---
+++ bin/CMakeFiles/myRythmFever.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/max/Documents/MyRythmFever/source/audioplayer.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/audioplayer.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/editor.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/editor.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/globalproperties.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/globalproperties.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/interface.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/interface.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/interface_button.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/interface_button.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/interface_label.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/interface_label.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/interface_numberfield.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/interface_numberfield.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/objectmanager.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/objectmanager.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/textrenderer.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/textrenderer.cpp.o"
+  "/home/max/Documents/MyRythmFever/source/timeline.cpp" "/home/max/Documents/MyRythmFever/bin/CMakeFiles/myRythmFever.dir/source/timeline.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../source"
+  "../MyRythmFever"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
