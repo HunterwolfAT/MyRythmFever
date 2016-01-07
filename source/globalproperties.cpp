6f88efb8ccf84ee2f9acd3b06185bb8899fe0083
Maximilian Siess <hunterwolf0815@gmail.com>
2016-01-07 12:52:51 +0100

    Update

--- source/globalproperties.cpp
+++ source/globalproperties.cpp
@@ -11,6 +11,7 @@ void GlobalProperties::updateResolution( int new_base_height, int new_base_width
 	true_window_width = new_base_width;
 	true_window_height = new_base_height;
 	screenscale_factor = true_window_height / base_window_height;
+	std::cout << "Set screenscale_factor to " << screenscale_factor << std::endl;
 	scaled_window_width = base_window_width * screenscale_factor;
 	scaled_window_height = base_window_height * screenscale_factor;
 	resolution_set = true;
